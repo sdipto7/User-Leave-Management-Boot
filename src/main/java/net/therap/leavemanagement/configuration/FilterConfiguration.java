61e4eba9485614407d590efa39341de832e313b9
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2022-01-19 14:59:48 +0600

    Added Filter Configurations

---
+++ src/main/java/net/therap/leavemanagement/configuration/FilterConfiguration.java
@@ -0,0 +1,72 @@
+package net.therap.leavemanagement.configuration;
+
+import net.therap.leavemanagement.filter.ExceptionFilter;
+import net.therap.leavemanagement.filter.InvalidSessionFilter;
+import net.therap.leavemanagement.filter.UserActivationFilter;
+import net.therap.leavemanagement.filter.ValidSessionFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author rumi.dipto
+ * @since 1/19/22
+ */
+@Configuration
+public class FilterConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<ExceptionFilter> exceptionFilter() {
+        FilterRegistrationBean<ExceptionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ExceptionFilter());
+        registrationBean.addUrlPatterns("/*");
+        
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<UserActivationFilter> userActivationFilter() {
+        FilterRegistrationBean<UserActivationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new UserActivationFilter());
+        registrationBean.addUrlPatterns(
+                "/dashboard",
+                "/user/teamLeadList",
+                "/user/developerList",
+                "/user/testerList",
+                "/user/details",
+                "/user/form",
+                "/user/submit",
+                "/notification",
+                "/leave/*",
+                "/logout",
+                "/success"
+        );
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<InvalidSessionFilter> invalidSessionFilter() {
+        FilterRegistrationBean<InvalidSessionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new InvalidSessionFilter());
+        registrationBean.addUrlPatterns(
+                "/dashboard",
+                "/user/*",
+                "/notification",
+                "/leave/*",
+                "/logout",
+                "/success"
+        );
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ValidSessionFilter> validSessionFilter() {
+        FilterRegistrationBean<ValidSessionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ValidSessionFilter());
+        registrationBean.addUrlPatterns("/", "/login");
+
+        return registrationBean;
+    }
+}
