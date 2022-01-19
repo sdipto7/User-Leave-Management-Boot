6cbbae2718be2f99ccb8475de88d28d926ec9a11
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2022-01-19 14:13:07 +0600

    Configured basic properties for the project

---
+++ src/test/java/net/therap/leavemanagement/UserLeaveManagementBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.therap.leavemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLeaveManagementBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
