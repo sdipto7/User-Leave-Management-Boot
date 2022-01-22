0eef2bf0efda2d069f32f45b29f1e5ff0dbcf784
Shahriar Rumi Dipto <rumi.dipto@therapservices.net>
2022-01-22 17:48:03 +0600

    Updated the resource location and the configuration

--- src/main/java/net/therap/leavemanagement/configuration/WebConfiguration.java
+++ src/main/java/net/therap/leavemanagement/configuration/WebConfiguration.java
@@ -1,11 +1,14 @@
 package net.therap.leavemanagement.configuration;
 
 import net.therap.leavemanagement.formatter.UserFormatter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * @author rumi.dipto
@@ -15,24 +18,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/styles/*").addResourceLocations("/styles/");
-        registry.addResourceHandler("/js/*").addResourceLocations("/js/");
+        registry.addResourceHandler("/styles/*").addResourceLocations("/WEB-INF/styles/");
+        registry.addResourceHandler("/js/*").addResourceLocations("/WEB-INF/js/");
     }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new UserFormatter());
     }
-
-//    @Bean
-//    public OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor() {
-//        return new OpenEntityManagerInViewInterceptor();
-//    }
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor());
-//    }
 }
